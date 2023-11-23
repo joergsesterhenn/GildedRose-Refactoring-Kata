472f1850e637926901f39a1b1ce37e8271d2186e
Jörg Sesterhenn <joerg.sesterhenn@gmail.com>
2023-11-23 21:37:18 +0100

    refactor: invert some if statements

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -4,33 +4,32 @@ def update_item_quality(item):
     aged_brie = item.name == "Aged Brie"
     backstage_pass = item.name == "Backstage passes to a TAFKAL80ETC concert"
     sulfuras = item.name == "Sulfuras, Hand of Ragnaros"
-    if not aged_brie and not backstage_pass:
-        if item.quality > 0:
-            if not sulfuras:
-                item.quality = item.quality - 1
-    else:
+
+    if aged_brie or backstage_pass:
         if item.quality < 50:
             item.quality = item.quality + 1
             if backstage_pass:
-                if item.sell_in < 11:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-                if item.sell_in < 6:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                if item.sell_in < 11 and item.quality < 50:
+                    item.quality = item.quality + 1
+                if item.sell_in < 6 and item.quality < 50:
+                    item.quality = item.quality + 1
+    else:
+        if item.quality > 0:
+            if not sulfuras:
+                item.quality = item.quality - 1
     if not sulfuras:
         item.sell_in = item.sell_in - 1
     if item.sell_in < 0:
-        if not aged_brie:
-            if not backstage_pass:
+        if aged_brie:
+            if item.quality < 50:
+                item.quality = item.quality + 1
+        else:
+            if backstage_pass:
+                item.quality = item.quality - item.quality
+            else:
                 if item.quality > 0:
                     if not sulfuras:
                         item.quality = item.quality - 1
-            else:
-                item.quality = item.quality - item.quality
-        else:
-            if item.quality < 50:
-                item.quality = item.quality + 1
 
 
 class GildedRose(object):
