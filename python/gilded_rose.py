b7c348c61cbde61f57dc7705993fb5a89356570b
Jörg Sesterhenn <joerg.sesterhenn@gmail.com>
2023-11-23 22:11:45 +0100

    refactor: extract more methods

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -2,30 +2,52 @@
 
 def update_item_quality(item):
     if is_aged_brie(item) or is_backstage_pass(item):
-        if item.quality < 50:
-            item.quality = item.quality + 1
-            if is_backstage_pass(item):
-                if item.sell_in < 11 and item.quality < 50:
-                    item.quality = item.quality + 1
-                if item.sell_in < 6 and item.quality < 50:
-                    item.quality = item.quality + 1
+        increase_quality(item)
     else:
-        if item.quality > 0:
-            if not is_sulfuras(item):
-                item.quality = item.quality - 1
+        decrease_quality(item)
     if not is_sulfuras(item):
-        item.sell_in = item.sell_in - 1
-    if item.sell_in < 0:
+        decrease_sell_in(item)
+    if sell_in_date_passed(item):
         if is_aged_brie(item):
-            if item.quality < 50:
-                item.quality = item.quality + 1
+            if quality_not_yet_maxed(item):
+                increase_quality_by_one(item)
         else:
             if is_backstage_pass(item):
-                item.quality = item.quality - item.quality
+                item.quality = 0
             else:
-                if item.quality > 0:
-                    if not is_sulfuras(item):
-                        item.quality = item.quality - 1
+                decrease_quality(item)
+
+
+def quality_not_yet_maxed(item):
+    return item.quality < 50
+
+
+def sell_in_date_passed(item):
+    return item.sell_in < 0
+
+
+def increase_quality_by_one(item):
+    item.quality = item.quality + 1
+
+
+def decrease_sell_in(item):
+    item.sell_in = item.sell_in - 1
+
+
+def decrease_quality(item):
+    if item.quality > 0:
+        if not is_sulfuras(item):
+            item.quality = item.quality - 1
+
+
+def increase_quality(item):
+    if quality_not_yet_maxed(item):
+        increase_quality_by_one(item)
+        if is_backstage_pass(item):
+            if item.sell_in < 11 and quality_not_yet_maxed(item):
+                increase_quality_by_one(item)
+            if item.sell_in < 6 and quality_not_yet_maxed(item):
+                increase_quality_by_one(item)
 
 
 def is_sulfuras(item):
